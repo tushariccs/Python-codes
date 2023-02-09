3e1da36d54f3a4c41cd5c7537b349d2d28852e8e
tushariccs <103886040+tushariccs@users.noreply.github.com>
2023-02-09 15:58:43 +0530

    Operators Chapter Done

---
+++ Telusko Python/Operator.py
@@ -0,0 +1,49 @@
+# x = 2
+# print(x)
+# y = 4
+# print(y)
+# a = x+y
+# print(a)
+# a = x-y
+# print(a)
+# a = x*y
+# print(a)
+# a = x/y
+# print(a)
+# a = x % y
+# print(a)
+
+# # we can assign value in many ways
+# x += 2
+# y += 2
+# # works with all type of operators
+# b, c = 5, 6
+# print(c)
+
+# # Unary Operator
+# n = 7
+# print(n)
+# # after apply in  -n it works like a unary operator
+# n = -n
+# print(n)
+
+# Relational Operators
+a = 6
+b = 7
+res = a > b
+print(res)
+a = 7
+res = a >= b
+print(res)
+
+res = a == b
+print(res)
+
+res = a != b
+print(res)
+
+res = a > 0 and b < 0
+print(res)
+
+res = a > 0 or b > 0
+print(res)
