36c6ea9f15f1162d64940333f7fb69a1afd524df
tushariccs <103886040+tushariccs@users.noreply.github.com>
2023-02-09 16:23:25 +0530

    Problems folder added

---
+++ Telusko Python/Problems/swap.py
@@ -0,0 +1,6 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+a, b = b, a
+print("The first number after swap: ", a)
+print("The second number after swap: ", b)
