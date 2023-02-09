60130aed2154f9fc9f99b4471de4302833ef77e7
tushariccs <103886040+tushariccs@users.noreply.github.com>
2023-02-09 16:12:58 +0530

    12 Lesson Done

---
+++ Telusko Python/NumberSystem.py
@@ -0,0 +1,13 @@
+a = 25
+print(bin(a))
+# O/P: 0b11001 0b is defined for binary system
+print(0b1010)
+# O/p:10
+print(oct(25))
+# O/P: 0o31 0b is defined for octane system
+print(hex(25))
+# 0x19 0x is defined for hex value
+print(hex(10))
+print(0xa)
+# 0xa
+# 10
