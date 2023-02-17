77c8cffa2065d0836656d80e57be7a12fad5e86f
tushariccs <103886040+tushariccs@users.noreply.github.com>
2023-02-17 09:07:38 +0530

    String is an imp topic

--- Akshit Sir/String.py
+++ Akshit Sir/String.py
@@ -51,3 +51,90 @@ greeting = "Good Morning"
 name = " John Doe"
 greeting = greeting + name
 print(greeting)
+
+# interation
+for char in name:
+    print(char)
+
+# check
+print("Morning" in greeting)
+
+# white spaces
+hello = "    hello".strip()
+print(hello)
+
+# upper and lower
+print(hello.upper())
+
+username = "TusHARiCCSSSsss"
+print(username.lower())
+
+# startswith
+akshit_email = "akshit.mathiwala@techbulls.com"
+tushar_email = "techbullstushar"
+aditya_email = "techbullsaditya"
+
+print(tushar_email.startswith("techbulls"))
+print(akshit_email.endswith("techbulls.com"))
+
+# find
+akshit_name = "mathaiwalaakshit"
+# to find index value of the character
+print(akshit_email.find("a"))
+# if character is not found it will return -1
+
+# SHIT
+s_index = akshit_name.find('s')
+print(s_index)
+splited_name = akshit_name[s_index:]
+print(splited_name)
+upper_splited_name = splited_name.upper()
+print(upper_splited_name)
+# AKSHIT
+# to find shit in name in upper case
+
+
+# count
+series_name = "The Woman in the House Across the Street from the Girl in the Window"
+print(series_name.count("e"))
+
+# replace
+print(series_name.replace("the", "anything"))
+print(series_name)
+
+
+# split
+print(series_name)
+print(series_name.split(" "))
+print(series_name.split(","))
+print(series_name.split("e"))
+splited_string_list = series_name.split()
+print(series_name)
+
+# join
+joining_list = ",".join(splited_string_list)
+print(joining_list)
+
+
+# if we don't use join and split and we want this o/p \
+# The,Woman,in,the,House,Across,the,Street,from,the,Girl,in,the,Window
+# then use replace(" ",",")
+
+a_list = ['a']*10
+print(a_list)
+a_list = "".join(a_list)
+print(a_list)
+# print(a_list.replace(",", ""))
+
+# another method to that
+a_string_2 = ""
+for a in a_list:
+    a_string_2 += a
+print(a_string_2)
+
+# % .format() f-string
+greeting = "Good Morning,"
+name = " John Doe"
+print(greeting + " "+name)
+print("{}whatever {} adads".format(greeting, name))
+print(f"{greeting}adadss{name}vikier{10+30}")
