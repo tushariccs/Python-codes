85a11b786a92b53d397467dba9842a10c1cc2a60
tushariccs <103886040+tushariccs@users.noreply.github.com>
2023-02-17 09:39:47 +0530

    Classes done

--- Akshit Sir/ClassesPython/class.py
+++ Akshit Sir/ClassesPython/class.py
@@ -9,3 +9,28 @@
 # iphone_qty = 5
 # iphone_total_amount = iphone_price * iphone_qty
 # print(iphone_total_amount)
+
+
+# class Product:
+#     pass
+
+
+# samsung = Product()
+# print(type(samsung))
+
+
+class Product:
+    def __init__(self, name, price, qty):
+        self.name = name
+        self.price = price
+        self.qty = qty
+
+    def calculate_total_amount(self):
+        return self.price * self.qty
+
+
+samsung = Product("Samsung FE", 50000, 10)
+iphone = Product("Iphone 14 Pro Max", 72000, 5)
+print(samsung.name)
+print(samsung.calculate_total_amount())
+print(iphone.calculate_total_amount())
