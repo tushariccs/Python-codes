526aebc8ef23592ba2a60995f6caa2e1d224d7bd
tushariccs <103886040+tushariccs@users.noreply.github.com>
2023-02-18 11:50:36 +0530

    Pattern finished

--- Telusko Python/15to25/PrintingPattern.py
+++ Telusko Python/15to25/PrintingPattern.py
@@ -1,4 +1,13 @@
-for i in range(4):
+# no of lines
+for i in range(0, 4):
+    # no of '#' to print
     for j in range(i+1):
         print("#", end=" ")
     print()
+
+# pattern output
+
+#  ->1 line i.e line is i 1 and '#' print is due to j loop
+# # ->2 line i.e line is i 2 and '#' print is due to j loop
+# # #  ->3 line i.e line is i 3 and '#' print is due to j loop
+# # # # -> 4 line i.e line is i 4 and '#' print is due to j loop
