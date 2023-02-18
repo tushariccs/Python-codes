eca9a037de9c099bfe5b7e1d085850dcc0befea3
tushariccs <103886040+tushariccs@users.noreply.github.com>
2023-02-18 12:03:04 +0530

    for else is completed

---
+++ Telusko Python/15to25/forelse.py
@@ -0,0 +1,14 @@
+list = [18, 16, 18, 19, 21, 89, 69]
+for lists in list:
+    # lists stores the value of list
+    # i.e lists = 18,16,18,19,21
+    # print(lists)
+    if (lists % 5 == 0):
+        print(lists)
+        break
+else:
+    # if there is no number which is divisble by 5
+    print("Not Found")
+
+
+# this is example of for else
